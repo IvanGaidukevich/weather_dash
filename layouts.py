543b9bc9569178af756518ee39a0d844101df3ad
IvanGaidukevich <gaydu7@gmail.com>
2025-04-14 21:37:35 +0300

    deployy

--- layouts.py
+++ layouts.py
@@ -4,7 +4,7 @@ from dash import dcc
 def create_layout():
     return dbc.Container([
         dbc.NavbarSimple(
-            brand="🌦 Прогноз  погоды :)))))) 🌦",
+            brand="🌦 Прогноз  погоды :))))))) 🌦",
             brand_href="#",
             color="primary",
             dark=True,
